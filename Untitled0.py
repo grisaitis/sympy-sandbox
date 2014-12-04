3ceb6ef5ad463d6ea6f6d4966732662cd98038ab
William Grisaitis <wgrisaitis@gmail.com>
2014-12-03 23:14:59 -0500

    initial code bits

---
+++ Untitled0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import sympy
+from sympy import symbol
+
+# <codecell>
+
+A = sympy.symbols('A', commutative=False)
+
+# <codecell>
+
+d = sympy.Function('d', commutative=False)
+
+# <codecell>
+
+
